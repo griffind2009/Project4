aeb7e88d55323d6c481f8374919a950768dca074
Domonique Griffin <griffind2009@gmail.com>
2016-11-27 18:55:08 -0500

    started angular

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,21 @@ angular
     "$stateProvider",
     Router
   ])
+  .controller([
+    "questionIndex",
+    [questionIndexController]
+  ])
+
+function questionIndexController () {
+  this.questions = Question.query()
+}
+
+function Router ($stateProvider) {
+  $stateProvider
+    .state("index", {
+      url: "/index",
+      templateUrl: "/public/js/ng-views/index.html",
+      controller: "questionIndex",
+      controllerAs: "vm"
+    })
+}
