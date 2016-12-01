415c45b9fc72f967ab947388f4f1f4fe3d316777
Domonique Griffin <griffind2009@gmail.com>
2016-12-01 15:32:45 -0500

    adding cross origin

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.use(bodyParser.json({ extended: true }))
 
 app.use(express.static(__dirname + '/public'))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/*?", function(req, res){
   res.render("index");
 });
