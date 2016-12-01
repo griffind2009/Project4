db311b6652a3819866dcf3d541173b1ebbac72c0
Domonique Griffin <griffind2009@gmail.com>
2016-12-01 15:59:24 -0500

    testing

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ app.use(bodyParser.json({ extended: true }))
 
 app.use(express.static(__dirname + '/public'))
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.get('/', function(req, res, next) {
-});
-
-app.post('/', function(req, res, next) {
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+//
+// app.get('/', function(req, res, next) {
+// });
+//
+// app.post('/', function(req, res, next) {
+// });
 
 app.get("/*?", function(req, res){
   res.render("index");
